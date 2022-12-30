ab88125cb97cfb89bb4398a14c8f7b9af4e5b279
shackeelck <shackeelck@gmail.com>
2022-12-30 14:38:45 +0500

    url

--- includes/functions-html.php
+++ includes/functions-html.php
@@ -10,8 +10,8 @@ function yourls_html_logo() {
 	?>
 	<header role="banner">
 	<h1>
-		<a href="<?php echo yourls_admin_url( 'index.php' ) ?>" title="YOURLS"><span>YOURLS</span>: <span>Y</span>our <span>O</span>wn <span>URL</span> <span>S</span>hortener<br/>
-		<img src="<?php yourls_site_url(); ?>/images/yourls-logo.svg?v=<?php echo YOURLS_VERSION; ?>" id="yourls-logo" alt="YOURLS" title="YOURLS" /></a>
+		<a href="<?php echo yourls_admin_url( 'index.php' ) ?>" title="YOURLS"><span>YOURLS</span>: <span>tcm url shortner</span><br/>
+		<img src="<?php yourls_site_url(); ?>/images/logo.ico" id="yourls-logo" alt="YOURLS" title="YOURLS" /></a>
 	</h1>
 	</header>
 	<?php
